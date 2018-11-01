7027e8284fba9d0e70ce4f4313d73774674466a7
Rubymoyes <moyesruby@gmail.com>
2018-11-01 15:27:25 +1300

    trying to create a 20 min timer that you can start and stop

---
+++ Timer-3/timer_three.js
@@ -0,0 +1,20 @@
+function startTimer(){
+    var counter = 5;
+    setInterval(function() {
+      counter--;
+      if (counter >= 0) {
+        span = document.getElementById("count");
+        span.innerHTML = counter;
+      }
+      if (counter === 0) {
+          alert('sorry, out of time');
+          clearInterval(counter);
+      }
+    }, 1000);
+  }
+
+  
+  $("#startClock").click(function(){
+      startTimer();
+   });
+  
