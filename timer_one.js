94d130595397190657b4de28ef9c297d9a560cad
Rubymoyes <moyesruby@gmail.com>
2018-11-01 14:52:30 +1300

    added working countdown timer, needs a start and pause button

--- timer_one.js
+++ timer_one.js
@@ -1,3 +1,5 @@
+var Timer = require('lib/easytimer/dist/easytimer.min.js')
+
 // Main stopwatch functions
 
 // function timeRemaining()
@@ -5,38 +7,3 @@
 // function startTimer()
 
 // function stopTimer()
-
-
-// Basic Timer - Seconds
-
-var timer = new Timer();
-timer.start();
-timer.addEventListener('secondsUpdated', function (e) {
-    $('#basicUsage').html(timer.getTimeValues().toString());
-});
-
-// Start, Pause, Stop and Reset Chronometer
-
-// var timer = new Timer();
-// $('#chronoExample .startButton').click(function () {
-//     timer.start();
-// });
-// $('#chronoExample .pauseButton').click(function () {
-//     timer.pause();
-// });
-// $('#chronoExample .stopButton').click(function () {
-//     timer.stop();
-// });
-// $('#chronoExample .resetButton').click(function () {
-//     timer.reset();
-// });
-// timer.addEventListener('secondsUpdated', function (e) {
-//     $('#chronoExample .values').html(timer.getTimeValues().toString());
-// });
-// timer.addEventListener('started', function (e) {
-//     $('#chronoExample .values').html(timer.getTimeValues().toString());
-// });
-// timer.addEventListener('reset', function (e) {
-//     $('#chronoExample .values').html(timer.getTimeValues().toString());
-// });
-
