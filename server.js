8898f0486e6c0f74a3ba32dc4fe3fcfdbc4b05ed
Karry <hello@devacademy.co.nz>
2018-11-08 11:12:04 +1300

    created server and index files for localhost

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const server = express()
+
+// Middleware
+
+server.engine('hbs', hbs({extname: 'hbs'}))
+server.set('view engine', 'hbs')
+server.use(express.urlencoded({extended: true}))
+
+// Routes
+
+module.exports = server
