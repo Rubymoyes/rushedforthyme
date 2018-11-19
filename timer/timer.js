74c3bcb195675d81f9c6c39867b52bf183ee3770
Rubymoyes <moyesruby@gmail.com>
2018-11-19 21:36:26 +1300

    added a bit of css, then deleted it, added all instructions for the timer into data.js and into the main file

--- timer/timer.js
+++ timer/timer.js
@@ -2,9 +2,21 @@
 
 let data = {
   "instructions": {
-      "19:58" : "Instruction 1",
-      "19:55" : "Instruction 2",
-      "19:50" : "Instruction number 3!"
+      "19:59" : "Firstly, pour the 4 cups of boiling water into the pot (and make sure the element is on)",
+      "19:30" : "Once semi-boiling, put the diced carrots. Time it for (approximately) four minutes from now. Obviously, the timer will tell you when it's done...",
+      "15:50" : "Before bringing the carrots out, drop the peas in quickly for a bit.",
+      "15:00" : "Bring the carrots and peas out and drain. Put aside for later use. Put the pot to the side as well, you won't be needing it anymore.",
+      "14:00" : "Heat up the wok over some high heat, and pour in the cooking oil. Let simmer for a couple of seconds.",
+      "13:30" : "Stir in the carrots and the peas. Feel free to add anything else left in your fridge - for example, spring or green onion, chopped peppers or anything else that you need to get rid of.",
+      "13:00" : "Pour in the egg mixture, and stir quickly to mix with the vegetables.",
+      "14:30" : "Once the eggs have taken on some colour, stir in the cooked rice and mix well. You can take a quick wine break, and have a chat to your flatmates.",
+      "09:00" : "Shake in some soy sauce (use your eyes and common sense) and toss well to coat the rice.",
+      "08:00" : "Chuck another little bit of sesame oil in for taste. Toss again.",
+      "07:00" : "Turn off the element, and put a little bit more soy sauce in (again, use your eyes) and toss it again.",
+      "06:00" : "Prepare to serve the food! Drizzle a little bit of chili oil over it (if you like a bit of heat) and then set aside for a bit to cool off.",
+      "05:00" : "This is your moment to call your flatmates for dinner. Get some utensils out (or don't, that's your prerogative, you did cook after all) and set up your eating area.",
+      "03:00" : "Get your glass of wine, clean up your space a bit, and remember to bring the sauces to the table/couch/beanbag.",
+      "1:00" : "You're finished!"
   }
 }
 
